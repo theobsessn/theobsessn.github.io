7f01807364553cadd0f4f4bb12e9c9b454972aa2
fatihturker <fatihturker35@gmail.com>
2026-05-13 22:54:50 +0300

    🎚️ Dynamic waveform: animated when speaking, flat line when silent + clean title

--- script.js
+++ script.js
@@ -43,6 +43,9 @@ let lastCueIndex = -1;
 
 function clearLines() {
     displayedLines.forEach(el => el.classList.add('fading'));
+    // Waveform goes idle during pauses
+    ivyWaveform.classList.add('idle');
+    ivySpeaking.classList.remove('active');
     setTimeout(() => {
         displayedLines.forEach(el => el.remove());
         displayedLines = [];
@@ -50,6 +53,10 @@ function clearLines() {
 }
 
 function showLine(text) {
+    // Waveform comes alive when speaking
+    ivyWaveform.classList.remove('idle');
+    ivySpeaking.classList.add('active');
+
     // Dim previous lines
     displayedLines.forEach(l => {
         l.classList.remove('highlight');
